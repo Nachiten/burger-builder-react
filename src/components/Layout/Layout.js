4b5a66b6217532a21ad41a443fa01e40a4122286
Ignacio Baptista [Notebook] <ibaptista@frba.utn.edu.ar>
2021-01-08 15:03:29 -0300

    Terminado Capitulo 151, adding hamburger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        mostrarSideDrawer: false
+    }
+
+    cerrarSideDrawerHandler = () => {
+        this.setState({ mostrarSideDrawer: false });
+    }
+
+    toggleSideDrawerHandler = () => {
+        const seMuestra = this.state.mostrarSideDrawer;
+        this.setState({ mostrarSideDrawer: !seMuestra });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar toggleSideDrawer={this.toggleSideDrawerHandler} />
+                <SideDrawer open={this.state.mostrarSideDrawer} closed={this.cerrarSideDrawerHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    };
+
+}
+
+export default Layout;
