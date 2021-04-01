584a61e90f235505ef2a07010f391e953035b620
Ignacio Baptista [Notebook] <ibaptista@frba.utn.edu.ar>
2021-04-01 13:56:36 -0300

    Terminado Capitulo 241, Handling form submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,40 +4,131 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
+import { RuleTester } from 'eslint';
 
-class NombreClase extends Component {
+class ContactData extends Component {
 
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name:{
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your name'
+                },
+                value:'',
+                validation: {
+                    required: true,
+                    valid: false
+                }
+            },
+            gender:{
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'selectGender', displayValue: 'Select your gender'},
+                        {value: 'male', displayValue: 'Male'},
+                        {value: 'female', displayValue: 'Female'}
+                    ]
+                },
+                value:'',
+                validation: {
+                    required: true,
+                    valid: false,
+                    defaultValue: 'selectGender'
+                }
+            },
+            street:{
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value:'',
+                validation: {
+                    required: true,
+                    valid: false
+                }
+            },
+            zipCode:{
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value:'',
+                validation: {
+                    required: true,
+                    valid: false
+                }
+            },
+            country:{
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Country'
+                },
+                value:'',
+                validation: {
+                    required: true,
+                    valid: false
+                }
+            },
+            email:{
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value:'',
+                validation: {
+                    required: true,
+                    valid: false
+                }
+            },
+            deliveryMethod:{
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'selectDelivery', displayValue: 'Select your delivery method'},
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value:'',
+                validation: {
+                    required: true,
+                    valid: false,
+                    defaultValue: 'selectDelivery'
+                }
+            },
         },
         loading: false
+    }
+
+    componentDidMount() {
+        console.log(this.state.orderForm);
     }
 
     orderHandler = (event) => {
         event.preventDefault();
         console.log(this.props.ingredients);
 
-        //alert('Continuamos!');
-
         this.setState({ loading: true });
+
+        const formData = {};
+
+        for (let formElementId in this.state.orderForm){
+            formData[formElementId] = this.state.orderForm[formElementId].value;
+        }
+
+        console.log(formData);
 
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Ignacio Baptista',
-                adress: {
-                    street: 'Corrientes 123',
-                    zipCode: '1414',
-                    country: 'Argentina'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+            orderData: formData
         }
 
         axios.post('/orders.json', order)
@@ -50,15 +141,66 @@ class NombreClase extends Component {
             });
     }
 
+    checkValidity(value, formElement){
+        let isValid = false;
+        
+        // if (formElement.validation.required){
+        //     if(formElement.elementType === 'input'){
+        //         isValid = value.trim() !== '';
+        //     } else if (formElement.elementType === 'select'){
+        //         isValid = value.trim() !== formElement.validation.defaultValue;
+        //     }
+        // }
+
+        // if (formElement.validation.minLength){
+        //     isValid = value.length >= formElement.validation.minLength;
+        // }
+        
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        console.log(event.target.value);
+
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.validation.valid = this.checkValidity(updatedFormElement.value, updatedFormElement)
+
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        console.log(updatedFormElement);
+        this.setState({orderForm: updatedOrderForm});
+    }
+
     render() {
 
+        const formElementsArray = [];
+
+        for (let key in this.state.orderForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
-            <form>
-                <input type="text" name="name" placeholder="Tu nombre" />
-                <input type="email" name="name" placeholder="Tu email" />
-                <input type="text" name="street" placeholder="Tu calle" />
-                <input type="text" name="postal" placeholder="Tu codigo postal" />
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            <form onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                    key={formElement.id}
+                    inputtype={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
+                ))}
+                <Button btnType="Success" >ORDER</Button>
             </form>
         );
 
@@ -75,4 +217,4 @@ class NombreClase extends Component {
     }
 }
 
-export default NombreClase;
+export default ContactData;
